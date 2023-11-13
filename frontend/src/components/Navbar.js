785297f78398a38fbf94a51cd06423d5d9efb45f
aruns05 <arun.s2k8@gmail.com>
2023-11-13 22:10:40 +0530

    Creating first branch

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+export function Navbar({ address, setView }) {
+    return (
+        <div className="navbar-container">
+            <div className="navbar">
+                <div onClick={() => setView("home")}>Home</div>
+                <div onClick={() => setView("profile")}>My Profile</div>
+            </div>
+            <div className="user-account">
+                Welcome {address}
+            </div>
+        </div>
+    )
+}
---
+++ frontend/src/components/WalletNotDetected.js
@@ -0,0 +1,10 @@
+export function WalletNotDetected() {
+    return (
+        <div className="container">
+            Wallet not detected. Please install
+            <a className="link" href="https://metamask.io/download/" target="_blank" rel="norferrer">
+                Metamask Browser Extension
+            </a>
+        </div>
+    )
+}
